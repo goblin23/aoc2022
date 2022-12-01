88eefc8a1c70b936802038e0739c2f8e0115280b
Guido Schöning <guido.schoening@gmail.com>
2022-12-01 12:34:40 +0100

    implemented dy 1

--- 1/1.rb
+++ 1/1.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
 
 if __FILE__ == $0
-    file = File.open "calories.txt"
-    puts file.readline
+    elves = []
+    file = File.open "/Users/gschoeni/repos/aoc2022/1/calories.txt"
+    input = file.read.split(/^\n/)
+    input.each do |line|
+        sum = 0
+        line.split.each do |number|
+            sum = sum + number.to_i
+        end
+        elves << sum
+    end
+    maxelves = 0
+    3.times do
+        maxelves = maxelves + elves.max
+        elves.delete(elves.max)
+    end
+    puts maxelves
 end
